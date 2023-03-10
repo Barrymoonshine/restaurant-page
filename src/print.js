8af1e5f5a738964752865ce572ddb7a4f388012b
Nick Sheward <nickshwrd@gmail.com>
2023-03-10 14:12:51 +0000

    Refactored pageLoad function to use entirely innerHTML for simplicity

--- src/print.js
+++ src/print.js
@@ -1,58 +1,46 @@
 export default function pageLoad() {
   const content = document.getElementById("content");
-  const header = document.createElement("div");
-  header.className = "header";
-  header.innerHTML = `
+
+  content.innerHTML = `
+  <div class="header"> 
   <ul>
     <li>Home</li>
     <li>Bookings</li>
     <li>Menu</li>
-  </ul>`;
-
-  const mainContainer = document.createElement("div");
-  mainContainer.className = "main-container";
-
-  const contentContainer = document.createElement("div");
-  contentContainer.className = "content-container";
-
-  const title = document.createElement("div");
-  title.className = "restaurant-title";
-  title.innerHtml = `<h2>Luigi's dinner<h2>
-  <h4>The best Italian in town - just like mama used to make it! </h4>`;
-
-  const customerReviews = document.createElement("div");
-  customerReviews.className = "customer-reviews";
-  customerReviews.innerHTML = `<p> Luigi's has the best green shells in Mushroom World! 
-  Me and my girl Peach feel much safer here than at Bowser's firey flame grill. </p> 
-  <p> It's a me Mario </p>`;
-
-  const openingTimes = document.createElement("div");
-  openingTimes.className = "opening-times";
-  openingTimes.innerHTML = ` <ul>
-  <li>Tuesday - Thursday: 6pm - 11pm</li>
-  <li>Friday - Saturday: 12pm - 1am </li>
-  <li>Sunday: 12pm - 7pm </li>
-  </ul>`;
-
-  const contactUs = document.createElement("div");
-  contactUs.className = "contact-us";
-  contactUs.innerHTML = `
-  <p>Address: 5 Pipe Lane, Grass Land, Mushroom World </p>
-  <p>Phone: 929-55-LUIGI </p>
-  <p>Email: luigi@yoshimail.com</p>
+  </ul>
+  </div>
+  <div class="main-container">
+    <div class="content-container">
+        <div class="title">
+            <h2>Luigi's dinner</h2>
+            <h4>The best Italian in town - just like mama used to make it! </h4>
+        </div>
+        <div class="customer-review">
+            <p> Luigi's has the best green shells in Mushroom World! 
+            Me and my girl Peach feel much safer here than at Bowser's firey flame grill. </p> 
+            <p> It's a me Mario </p>
+        </div>
+        <div class="opening-times">
+            <h3>Opening times</h3>
+            <ul>
+                <li>Tuesday - Thursday: 6pm - 11pm</li>
+                <li>Friday - Saturday: 12pm - 1am </li>
+                <li>Sunday: 12pm - 7pm </li>
+            </ul>
+        </div>
+        <div class="contact-us">
+            <h3>Contact us</h3>
+            <p>Address: 5 Pipe Lane, Grass Land, Mushroom World </p>
+            <p>Phone: 929-55-LUIGI </p>
+            <p>Email: luigi@yoshimail.com</p>
+        </div>
+    </div>  
+   </div>
+   <div class="footer">
+    <p> © Made by
+    <a href="https://github.com/Barrymoonshine">Barry Moonshine</a> for
+    <a href="https://www.theodinproject.com/">The Odin Project</a>
+    </p>
+    </div>
   `;
-
-  const footer = document.createElement("footer");
-  footer.className = "footer";
-  footer.innerHTML = `
-  <p>
-  © Made by
-  <a href="https://github.com/Barrymoonshine">Barry Moonshine</a> for
-  <a href="https://www.theodinproject.com/">The Odin Project</a>
-  </p>
-  `;
-
-  content.append(header, mainContainer, footer);
-  mainContainer.appendChild(contentContainer);
-  contentContainer.append(title, customerReviews, openingTimes, contactUs);
 }
